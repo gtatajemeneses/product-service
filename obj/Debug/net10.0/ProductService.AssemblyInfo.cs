5bb1febb4e9f14d27ef17454238349b0d376b23b
Gerardo <gerardotataje@hotmail.com>
2026-07-17 08:12:36 -0500

    ci: add deployment pipeline for product service

--- obj/Debug/net10.0/ProductService.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfe2413a00f34adc89f12544e9d83e27428e2b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
